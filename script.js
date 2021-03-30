35f5394e1f18b124d7ece7575f947baad47f045f
EdmilsonSant <81649592+EdmilsonSant@users.noreply.github.com>
2021-03-30 20:13:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-//const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 	
 function busca(){
     
     $.ajax({
         
         async: true,
-        url: "https://api-java-top.herokuapp.com/top",
+        url: proxyurl + "https://api-java-top.herokuapp.com/top",
         method: "GET",
 
         success:function(response){
